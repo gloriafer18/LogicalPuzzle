e257dbe7cd3af01668142b07417d74ace0ef2231
Gloria Fernandez <gloriafer18@gmail.com>
2014-10-09 16:06:11 -0500

    Nueva Version

--- Puzzle/src/main/java/com/sample/Celda.java
+++ Puzzle/src/main/java/com/sample/Celda.java
@@ -10,44 +10,88 @@ public class Celda {
 		public static final char VACIA = '-';
 		
 		private char  value =' '; //Representa el valor de la celda
-	    private CellRow   cellRow; // Representa el indice de la fila
-	    private CellCol   cellCol; // Representa el indice de la Columna
-	    private CellProf cellProf;  // Representa el indice de la profundidad
-		
+	    private int   cellRow; // Representa el indice de la fila
+	    private int cellCol; // Representa el indice de la Columna
+	    private int cellProf;  // Representa el indice de la profundidad
+	    private String insRow;
+	    private String insCol;
+	    private String insProf;
 	    
-	    public char getValue() {
+	    
+	    
+	    //Nuevas variables
+	    
+	    public static final int DOMINIO_1 = 0;
+        public static final int DOMINIO_2 = 1;
+        public static final int DOMINIO_3 = 2;
+        private String dominio1;
+        private String dominio2;
+        private String dominio3;
+                
+        char[][][] cubo = new char[4][4][4];
+        
+        
+        
+	    
+	    public String getInsRow() {
+			return insRow;
+		}
+		public void setInsRow(String insRow) {
+			this.insRow = insRow;
+		}
+		public String getInsCol() {
+			return insCol;
+		}
+		public void setInsCol(String insCol) {
+			this.insCol = insCol;
+		}
+		public String getInsProf() {
+			return insProf;
+		}
+		public void setInsProf(String insProf) {
+			this.insProf = insProf;
+		}
+		public int getCellRow() {
+			return cellRow;
+		}
+		public void setCellRow(int cellRow) {
+			this.cellRow = cellRow;
+		}
+		public int getCellCol() {
+			return cellCol;
+		}
+		public void setCellCol(int cellCol) {
+			this.cellCol = cellCol;
+		}
+		public int getCellProf() {
+			return cellProf;
+		}
+		public void setCellProf(int cellProf) {
+			this.cellProf = cellProf;
+		}
+		public char getValue() {
 
 			return value;
 		}
 		public void setValue(char value) {
 			this.value = value;
 		}
-		public CellRow getCellRow() {
-			return cellRow;
-		}
-		public void setCellRow(CellRow cellRow) {
-			this.cellRow = cellRow;
-		}
-		public CellCol getCellCol() {
-			return cellCol;
-		}
-		public void setCellCol(CellCol cellCol) {
-			this.cellCol = cellCol;
-		}
-		
-		
-		public CellProf getCellProf() {
-			return cellProf;
-		}
-		public void setCellProf(CellProf cellProf) {
-			this.cellProf = cellProf;
-		}
 		
 		//METODOS
 		
-		public void makeReferences(CellRow row, CellCol col, CellProf prof) {
+		public void makeReferences(int row, int col, int prof) {
 	        this.cellRow = row;
 	        this.cellCol = col;
 	        this.cellProf = prof;	        
 	    }		
+		
+		public Celda(int row, int col, int prof, String insr, String insc, String insp){
+			this.cellRow = row;
+	        this.cellCol = col;
+	        this.cellProf = prof;
+	        this.insRow = insr;
+	        this.insCol = insc;
+	        this.insProf = insp;			
+	        value=this.VACIA;
+		}		
 }
--- Puzzle/src/main/java/com/sample/IniciarJuego.java
+++ Puzzle/src/main/java/com/sample/IniciarJuego.java
@@ -7,12 +7,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import com.sample.*;
+import com.sample.DroolsTest.Message;
+
+import org.kie.api.runtime.rule.FactHandle;
+
 
 /**
  * @author Asistente
  *
  */
 public class IniciarJuego {
+	
+	public static IniciarJuego iniciajuego;
+	
 
 	/**
 	 * @param args
@@ -22,28 +34,39 @@ public class IniciarJuego {
 		
 		Properties puzzle = new Properties();
 		InputStream in = IniciarJuego.class.getResourceAsStream("puzzle.properties");
-		 char[][][] numbers = new char[4][4][4];
+		
+		 Celda[][][] cubo = new Celda[4][4][4];
+		 
+		 //char[][][] valores = new char[4][4][4];
+		 //Celda objPuzzle = new Celda();
+		 
+		 //Dominio 1
+		 
+		 String[] instancias_D1 = new String[4];
+	     String[] instancias_D2 = new String[4];
+	     String[] instancias_D3 = new String[4];
 		
 		try {
+			//CARGAMOS LOS DATOS DEL LOGICAL PUZZLE
 	        puzzle.load(in);
-	        //Defino los dominios seg�n el archivo .properties
-	        String D1=puzzle.getProperty("Dominio1");
+	        //Defino los dominios según el archivo .properties
+	        String D1=puzzle.getProperty("Dominio1");//
 	        String D2=puzzle.getProperty("Dominio2");
 	        String D3=puzzle.getProperty("Dominio3");
 	        
 	        //Instancias de cada dominio	        
-	        String i1d1 = puzzle.getProperty("i1d1");
-	        String i2d1 = puzzle.getProperty("i2d1");
-	        String i3d1 = puzzle.getProperty("i3d1");
-	        String i4d1 = puzzle.getProperty("i4d1");
-	        String i1d2 = puzzle.getProperty("i1d2");
-	        String i2d2 = puzzle.getProperty("i2d2");
-	        String i3d2 = puzzle.getProperty("i3d2");
-	        String i4d2 = puzzle.getProperty("i4d2");
-	        String i1d3 = puzzle.getProperty("i1d3");
-	        String i2d3 = puzzle.getProperty("i2d3");
-	        String i3d3 = puzzle.getProperty("i3d3");
-	        String i4d3 = puzzle.getProperty("i4d3");
+	        instancias_D1[0] = puzzle.getProperty("i1d1");
+	        instancias_D1[1] = puzzle.getProperty("i2d1");
+	        instancias_D1[2] = puzzle.getProperty("i3d1");
+	        instancias_D1[3] = puzzle.getProperty("i4d1");
+	        instancias_D2[0] = puzzle.getProperty("i1d2");
+	        instancias_D2[1] = puzzle.getProperty("i2d2");
+	        instancias_D2[2] = puzzle.getProperty("i3d2");
+	        instancias_D2[3] = puzzle.getProperty("i4d2");
+	        instancias_D3[0] = puzzle.getProperty("i1d3");
+	        instancias_D3[1] = puzzle.getProperty("i2d3");
+	        instancias_D3[2] = puzzle.getProperty("i3d3");
+	        instancias_D3[3] = puzzle.getProperty("i4d3");
 	        
 	        //Clues
 	        String C1=puzzle.getProperty("Clue1");
@@ -51,31 +74,88 @@ public class IniciarJuego {
 	        String C3=puzzle.getProperty("Clue3");
 	        String C4=puzzle.getProperty("Clue4");
 	        
-	        //Imprime los valores del archivo .properties
+/*	        //Imprime los valores del archivo .properties
 	        for (String key : puzzle.stringPropertyNames()) {
                 String value = puzzle.getProperty(key);
                 System.out.println("Key:- " + key + " Value:- " + value);
-            }
+            }*/	        
+	        	        
+	        //Instanciamos la clase celda
 	        
-	        //Llenar el tablero con los datos
+	        //Celda celda=new Celda();
 	        
-	        for (int prof=0; prof < 4; prof ++){//Este for representa la profundidad
-	        	for (int col=0; col < 4; col ++){ //Este for representa las instancias de cada dominio
-	        		for (int fil=0; fil< 4; fil ++){//Este for representa el valor de cada celda
-	        			numbers[prof][col][fil]= '-';
-	        			
-	        		} //Cierra for Valor	        		
-	        	}//Cierra for instancia
-	        }//Cierra for dominio
 	        
-	        //Identificar los elementos de cada clave
-/*	        if(){
-	        	 C1 = C1.substring(C1.indexOf(sTextoBuscado)+sTextoBuscado.length(),sTexto.length());
-	        	
-	        }*/
+	        //CARGAMOS LA BASE DE CONOCIMIENTO 
+	        //GO	        
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+        	
+        	//Setting s000 = new Setting(0, 0,0, '-');
+	        //FactHandle fh000 = kSession.insert(s000);
+	        //this.create();
+			
+			
+			//1. INICIALIZAMOS LAS CELDAS VACIAS 
+	        for (int fil=0; fil < 4; fil ++){//Este for representa la filas ----- Dominio1
+	        	for (int col=0; col < 4; col ++){ //Este for representa las columnas --- Dominio2
+	        		for (int prof=0; prof< 4; prof ++){//Este for representa las filas --- Dominio3
+	        	        //numCol=new CellCol(col);			
+	        			//numRow=new CellRow(fil);
+	        			//numprof=new CellProf(prof);
+	        			//celda.setValue('-');
+	        			//instancia de una celda
+	        			Celda c=new Celda(fil, col,prof,instancias_D1[fil],instancias_D2[col], instancias_D3[prof]);
+	        			cubo[fil][col][prof]= c;	        			
+	        			//System.out.println(numbers[prof][col][fil]= '-');
+	        			//Este objeto define el valor de una celda
+	        			kSession.insert(c);	        			
+	        			//Todas se inicializan vacias
+	        		} //Cierra for profundidad
+	        	}//Cierra for columna
+	        }//Cierra for fila
 	        
+	        
+	        
+	        //VALIDAR CLUE1
+	        
+	    	//System.out.println("2.) ANALIZAMOS LOS CLUES");
+	        //Es necesario partir la cadena con la clave para identificar (Revidar documento): 
+	        //Dc = Dominio de Comparación
+	        //Dd = Dominio de la Derecha
+	        //Di = Dominio de la Izquierda
+	        //id = Instancia de la derech
+	        //ii = Instacia de la izquierada
 
-	        in.close();
+	        
+	        //Igualdad
+	        
+	        StringTokenizer tokensc1 = new StringTokenizer(C1, "==");
+	        while(tokensc1.hasMoreTokens()){
+	        	System.out.println(tokensc1.nextToken());
+	        }
+	        
+	        
+	        //VALIDAR CLUE2
+	        //Comparación	        
+	        StringTokenizer tokensc2 = new StringTokenizer(C2,"<");
+	        
+	        //VALIDAR CLUE3
+	        //Dinjunto
+	        
+	        StringTokenizer tokensc3 = new StringTokenizer(C3);
+	        
+	        //VALIDAR CLUE34
+	        //Comparacion Menor	        
+	        StringTokenizer tokensc4 = new StringTokenizer(C4);
+
+	        
+	        
+	        
+	        //kSession.delete(fh000);	        
+            kSession.fireAllRules();
+            in.close();
+            
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
